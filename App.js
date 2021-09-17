5d9f61cb396432038f7779ec95b00bb28c09744f
mike <apolloproductionsdj@gmail.com>
2021-09-17 10:15:56 -0700

    added in <NavOptions /> with data to render through and display onto HomeScreen with styling

--- App.js
+++ App.js
@@ -4,14 +4,14 @@ import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
 import HomeScreen from './screens/HomeScreen';
 import { store } from './store';
-import tw from 'tailwind-react-native-classnames';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
     <Provider store={store}>
-      <View>
+      <SafeAreaProvider>
         <HomeScreen />
-      </View>
+      </SafeAreaProvider>
     </Provider>
   );
 }
---
+++ components/NavOptions.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
+import { Icon } from 'react-native-elements/dist/icons/Icon';
+import tw from 'tailwind-react-native-classnames';
+
+const NavOptions = () => {
+
+    const data = [
+        {
+            id: '123',
+            title: 'Get a ride',
+            image: 'https://links.papareact.com/3pn',
+            screen: "MapScreen",
+        },
+        {
+            id: "456",
+            title: "Order food",
+            image: "https://links.papareact.com/28w",
+            screen: "EatsScreen",
+        },
+    ]
+    return (
+        <View>
+            <FlatList
+                keyExtractor={(item) => item.id}
+                data={data}
+                horizontal
+                renderItem={({ item }) => (
+                    <TouchableOpacity
+                        style={tw`p-2 pt-6 pb-8 bg-gray-200 m-2 w-40`}
+                    >
+                        <View>
+                            <Image
+                                style={{ height: 120, width: 120, resizeMode: 'contain' }}
+                                source={{
+                                    uri: item.image
+                                }}
+                            />
+                            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+                            <Icon
+                                style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+                                name="arrowright" color="white" type="antdesign" />
+                        </View>
+                    </TouchableOpacity>
+                )}
+            />
+        </View>
+    )
+}
+
+export default NavOptions
+
