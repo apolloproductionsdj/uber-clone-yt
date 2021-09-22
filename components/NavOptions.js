f22c481837cf3a4f8ec2ba31f1f1d1870f6a2490
mike <apolloproductionsdj@gmail.com>
2021-09-21 21:43:19 -0700

    added RideOptionsCard and NavigateCard sot aht the Stack.Navigator inside of MapScreen can navigate to those component screens

--- components/NavOptions.js
+++ components/NavOptions.js
@@ -2,10 +2,14 @@ import { useNavigation } from '@react-navigation/core';
 import React from 'react';
 import { FlatList, Image, Text, TouchableOpacity, View } from 'react-native';
 import { Icon } from 'react-native-elements/dist/icons/Icon';
+import { useSelector } from 'react-redux';
 import tw from 'tailwind-react-native-classnames';
+import { selectOrigin } from '../slices/navSlice';
 
 const NavOptions = () => {
     const navigation = useNavigation();
+
+    const origin = useSelector(selectOrigin)
 
     const data = [
         {
@@ -31,8 +35,9 @@ const NavOptions = () => {
                     <TouchableOpacity
                         onPress={() => navigation.navigate(item.screen)}
                         style={tw`p-2 pt-6 pb-8 bg-gray-200 m-2 w-40`}
+                        disabled={!origin}
                     >
-                        <View>
+                        <View style={tw`${!origin && "opacity-20"}`}>
                             <Image
                                 style={{ height: 120, width: 120, resizeMode: 'contain' }}
                                 source={{
---
+++ components/RideOptionsCard.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const RideOptionsCard = () => {
+    return (
+        <View>
+            <Text>I am the Ride Bitch</Text>
+        </View>
+    )
+}
+
+export default RideOptionsCard
+
+const styles = StyleSheet.create({})
